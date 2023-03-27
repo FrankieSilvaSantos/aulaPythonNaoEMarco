89f432554b22ca47975a2b1bc7437591b48d8c97
Frankie Silva Santos <109253810+FrankieSilvaSantos@users.noreply.github.com>
2023-03-27 20:20:02 -0300

    Add files via upload

---
+++ character27-03.py
@@ -0,0 +1,5 @@
+character = str(input("ENtre o nome com mais de 3 characteres "))
+while(len(character)<3):
+   
+    character = str(input("ENtre o nome com mais de 3 characteres "))
+print("Nome correto",character)
